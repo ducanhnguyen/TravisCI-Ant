880a7ef9132a29abe11d091671b9929aa6b53bf3
ducanhnguyen <anhnd.56@gmail.com>
2018-04-24 00:37:29 +0700

    initial

---
+++ src/a.java
@@ -0,0 +1,11 @@
+
+
+public class a {
+	public static float divide(int a, int b) {
+		return a*1.0f / b;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(divide(1, 2));
+	}
+}
---
+++ src/testTest.java
@@ -0,0 +1,10 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class testTest {
+
+	@Test
+	public void testDivide() {
+		Assert.assertEquals(1, 1);
+	}
+}
