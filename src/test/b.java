8aa7bf89c49206522d46aa2f4d23d74520df48f1
ducanhnguyen <anhnd.56@gmail.com>
2018-04-24 01:12:06 +0700

    add travis

--- src/test/b.java
+++ src/test/b.java
@@ -2,10 +2,14 @@ package test;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class testTest {
+public class b {
 
 	@Test
 	public void testDivide() {
 		Assert.assertEquals(1, 1);
+	}
+	@Test
+	public void testDivide1() {
+		Assert.assertEquals(1, 2);
 	}
 }
