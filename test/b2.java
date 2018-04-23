d1bf0a7828ab75e16df7dcbe334ca6bec6123f28
ducanhnguyen <anhnd.56@gmail.com>
2018-04-24 01:18:52 +0700

    add travis

---
+++ test/b2.java
@@ -0,0 +1,15 @@
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class b2 {
+
+	@Test
+	public void testDivide() {
+		Assert.assertEquals(1, 1);
+	}
+	@Test
+	public void testDivide1() {
+		Assert.assertEquals(1, 2);
+	}
+}
